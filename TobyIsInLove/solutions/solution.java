3e07d39b16d91e91bfab024705a95173c275f18b
Jhon Jimenez <jhonb3r@gmail.com>
2018-05-21 20:07:38 -0500

    Update solution.java

--- TobyIsInLove/solutions/solution.java
+++ TobyIsInLove/solutions/solution.java
@@ -6,46 +6,49 @@ import java.util.StringTokenizer;
 
 public class solution {
 
-	static class Scanner{
-		BufferedReader br=null;
-		StringTokenizer tk=null;
-		public Scanner(){
-			br=new BufferedReader(new InputStreamReader(System.in));
-		}
-		public String next() throws IOException{
-			while(tk==null || !tk.hasMoreTokens()) {
-				String cad = br.readLine();
-				if (cad == null)
-					return null;
-				tk=new StringTokenizer(cad);
-			}
-			return tk.nextToken();
-		}
-		public int nextInt() throws NumberFormatException, IOException{
-			return Integer.valueOf(next());
-		}
-		public long nextLong() throws NumberFormatException, IOException{
-			return Long.valueOf(next());
-		}
-		public double nextDouble() throws NumberFormatException, IOException{
-			return Double.valueOf(next());
-		}
-	}
-	
-	public static void main(String args[]) throws NumberFormatException, IOException {
-		Scanner sc = new Scanner();
-		int n = sc.nextInt();
-		
-		for(int i = 0; i < n; i++) {
-			String s = sc.next();
+  static class Scanner{
+    BufferedReader br=null;
+    StringTokenizer tk=null;
+    public Scanner(){
+      br=new BufferedReader(new InputStreamReader(System.in));
+    }
+    public String next() throws IOException{
+      while(tk==null || !tk.hasMoreTokens()) {
+        String cad = br.readLine();
+        if (cad == null)
+          return null;
+        tk=new StringTokenizer(cad);
+      }
+      return tk.nextToken();
+    }
+    public int nextInt() throws NumberFormatException, IOException{
+      return Integer.valueOf(next());
+    }
+    public long nextLong() throws NumberFormatException, IOException{
+      return Long.valueOf(next());
+    }
+    public double nextDouble() throws NumberFormatException, IOException{
+      return Double.valueOf(next());
+    }
+  }
+
+  public static void main(String args[]) throws NumberFormatException, IOException {
+    Scanner sc = new Scanner();
+    int n = sc.nextInt();
+
+    for(int i = 0; i < n; i++) {
+      String s = sc.next();
       int ind = 0;
       int cnt = 0;
       while (ind != -1) {
         ind = s.indexOf("love", ind);
-        if (ind != -1) cnt ++;
+        if (ind != -1) {
+          cnt ++;
+          ind ++;
+        }
       }
 
       System.out.println(cnt);
-		}
-	}
+    }
+  }
 }
